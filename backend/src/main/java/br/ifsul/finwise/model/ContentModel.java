5c0c3452f68629e2623d2854b27e173b1bb64139
Rafael Carrera <121832592+rafaelcarrera10@users.noreply.github.com>
2025-11-05 08:31:13 -0300

    Comentario

--- backend/src/main/java/br/ifsul/finwise/model/ContentModel.java
+++ backend/src/main/java/br/ifsul/finwise/model/ContentModel.java
@@ -7,12 +7,12 @@ import java.util.Objects;
 @Table(name = "content")
 public class ContentModel {
 
-    // -------------------- RELACIONAMENTO --------------------
+    // RELACIONAMENTO
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "autor", nullable = false)
     private UserModel autor;
 
-    // -------------------- VARIÁVEIS --------------------
+    // VARIÁVEIS
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,7 +26,7 @@ public class ContentModel {
     @Column(name = "description", nullable = false)
     private String description;
 
-    // -------------------- CONSTRUTORES --------------------
+    // CONSTRUTORES
     public ContentModel() {
     }
 
@@ -36,7 +36,7 @@ public class ContentModel {
         this.description = description;
     }
 
-    // -------------------- GETTERS E SETTERS --------------------
+    // GETTERS E SETTERS
     public Long getId() {
         return id;
     }
@@ -77,7 +77,7 @@ public class ContentModel {
         this.description = description;
     }
 
-    // -------------------- HASHCODE E EQUALS --------------------
+    // HASHCODE,EQUALS E TOSTRING
     @Override
     public int hashCode() {
         return Objects.hash(id);
@@ -90,8 +90,7 @@ public class ContentModel {
         ContentModel other = (ContentModel) obj;
         return Objects.equals(id, other.id);
     }
-
-    // -------------------- TOSTRING --------------------
+    
     @Override
     public String toString() {
         return "ContentModel: id=" + (id != null ? id : "null") +
